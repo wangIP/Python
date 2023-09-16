58cac9bf39219b31db4f504280fec3557b570c14
wang <johns10166@gmail.com>
2023-09-16 21:53:01 +0900

    new day1

---
+++ day1/day1.py
@@ -0,0 +1,18 @@
+# # # a = 17
+# # # b = 3
+# # # # c = a/b #5.666666666666667
+# # # # c= a//b #5
+# # # c=a%b #2
+# # # print(c)   
+# # a = "dos\t"
+# # print(a)
+# # print('D:\work\programming')
+# # print(r'D:\work\programming')
+# # c = 'py''thon'
+# # print(c)
+# c = 'python'
+# # print(c[0:2])#py
+# # print(c[4:])#on
+# print(c[2:5])#tho
+c = ['1','2','3','4','5','6']
+print(c[2:4])#
